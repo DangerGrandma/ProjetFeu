177f89315033bcaee4132407ad9fe5edc27cc4aa
Alex <alexjolic@hotmail.com>
2019-04-20 18:38:50 -0400

    Version exemple pour Linds

--- src/essentiels/MainFrame.java
+++ src/essentiels/MainFrame.java
@@ -13,6 +13,10 @@ import java.awt.Container;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JEditorPane;
+import javax.swing.JTextField;
 
 public class MainFrame extends JFrame {
 	
@@ -20,17 +24,33 @@ public class MainFrame extends JFrame {
 	public MainFrame() {
 		
 		super("Feu Intelligent");
+		getContentPane().setEnabled(false);
 		getContentPane().setLayout(null);
 		
-		Chrono caca = new Chrono();
-		caca.setBounds(155, 113, 156, 100);
-		
 		PaneauFeu test = new PaneauFeu();
-		test.setBounds(321, 124, 58, 89);
+		test.setBounds(10, 48, 58, 89);
 		
 		Container c = getContentPane();
-		c.add(caca);
 		c.add(test);
+		
+		Chrono caca = new Chrono();
+		caca.setBounds(285, 48, 170, 124);
+		getContentPane().add(caca);
+		
+		JLabel lblNewLabel = new JLabel("Menu");
+		lblNewLabel.setFont(new Font("Vivaldi", Font.BOLD | Font.ITALIC, 25));
+		lblNewLabel.setBounds(0, 0, 74, 60);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblChronomtre = new JLabel("Chronom\u00E8tre");
+		lblChronomtre.setFont(new Font("Vivaldi", Font.BOLD | Font.ITALIC, 25));
+		lblChronomtre.setBounds(309, 4, 120, 52);
+		getContentPane().add(lblChronomtre);
+		
+		JEditorPane editorPane = new JEditorPane();
+		editorPane.setBackground(new Color(47, 79, 79));
+		editorPane.setBounds(0, 0, 584, 90);
+		getContentPane().add(editorPane);
 		
 		setSize(600, 500);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
